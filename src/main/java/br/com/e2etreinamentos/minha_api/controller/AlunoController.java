64a4196adf58cce7dabf621b6eb1987264f3afea
repoe2e <professores@e2etreinamentos.com.br>
2024-11-07 16:22:39 -0300

    ok

--- src/main/java/br/com/e2etreinamentos/minha_api/controller/AlunoController.java
+++ src/main/java/br/com/e2etreinamentos/minha_api/controller/AlunoController.java
@@ -3,9 +3,11 @@ package br.com.e2etreinamentos.minha_api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +29,24 @@ public class AlunoController {
 	}
 
 	// Consultar aluno por CPF
-    @GetMapping("/{cpf}")
-    public ResponseEntity<Aluno> consultarAluno(@PathVariable String cpf) {
-        return alunoService.consultarPorCpf(cpf)
-                .map(aluno -> new ResponseEntity<>(aluno, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+	@GetMapping("/{cpf}")
+	public ResponseEntity<Aluno> consultarAluno(@PathVariable String cpf) {
+		return alunoService.consultarPorCpf(cpf).map(aluno -> new ResponseEntity<>(aluno, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 
+	// Excluir aluno por CPF
+	@DeleteMapping("/{cpf}")
+	public ResponseEntity<String> excluirAluno(@PathVariable String cpf) {
+		alunoService.excluir(cpf);
+		return new ResponseEntity<>("Aluno excluído com sucesso!", HttpStatus.OK);
+	}
+	
+	// Atualizar aluno
+		@PutMapping("/{cpf}")
+		public ResponseEntity<String> atualizarAluno(@PathVariable String cpf, @RequestBody Aluno alunoAtualizado) {
+			alunoService.atualizar(cpf,alunoAtualizado);
+			return new ResponseEntity<>("Aluno atualizado com sucesso!", HttpStatus.OK);
+		}
 
 }
--- src/main/java/br/com/e2etreinamentos/minha_api/service/AlunoService.java
+++ src/main/java/br/com/e2etreinamentos/minha_api/service/AlunoService.java
@@ -12,23 +12,31 @@ import br.com.e2etreinamentos.minha_api.repository.AlunoRepository;
 @Service
 public class AlunoService {
 
-    @Autowired
-    private AlunoRepository alunoRepository;
+	@Autowired
+	private AlunoRepository alunoRepository;
 
-    public List<Aluno> listarTodos() {
-        return alunoRepository.findAll();
-    }
+	public List<Aluno> listarTodos() {
+		return alunoRepository.findAll();
+	}
 
-    public Optional<Aluno> consultarPorCpf(String cpf) {
-        return alunoRepository.findCpf(cpf);
-    }
+	public Optional<Aluno> consultarPorCpf(String cpf) {
+		return alunoRepository.findCpf(cpf);
+	}
 
-    public void cadastrar(Aluno aluno) {
-        alunoRepository.save(aluno);
-    }
+	public void cadastrar(Aluno aluno) {
+		alunoRepository.save(aluno);
+	}
 
- 
-    public void excluir(String cpf) {
-        alunoRepository.findCpf(cpf).ifPresent(alunoRepository::delete);
-    }
+	public void excluir(String cpf) {
+		alunoRepository.findCpf(cpf).ifPresent(alunoRepository::delete);
+	}
+
+	public void atualizar(String cpf, Aluno alunoAtualizado) {
+		alunoRepository.findCpf(cpf).ifPresent(aluno ->{
+			aluno.setNome(alunoAtualizado.getNome());
+			aluno.setSobrenome(alunoAtualizado.getSobrenome());
+			aluno.setEmail(alunoAtualizado.getEmail());
+			aluno.setTurma(alunoAtualizado.getTurma());
+		});
+	}
 }
