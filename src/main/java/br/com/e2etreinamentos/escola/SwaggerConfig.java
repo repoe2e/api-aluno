712aaba80005b515ea1af30e1307062db8bca652
repoe2e <professores@e2etreinamentos.com.br>
2024-11-27 15:22:45 -0300

    atualizado

--- src/main/java/br/com/e2etreinamentos/escola/SwaggerConfig.java
+++ src/main/java/br/com/e2etreinamentos/escola/SwaggerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+
+
 @Configuration
 public class SwaggerConfig {
 
